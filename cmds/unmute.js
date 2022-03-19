36bba963d7ad0d41db558a18a6dfd571882a0879
Dezkrazzer <uqie.vanmadiun@gmail.com>
2022-03-19 23:17:41 +0700

    fix: unmute command

--- cmds/unmute.js
+++ cmds/unmute.js
@@ -4,7 +4,7 @@ const db = require("quick.db")
 exports.run = async (client, message, args) => {
     let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     let muteRole = message.guild.roles.cache.find(r => r.name === "Muted")
-    let muted = await client.mongo.get(`isMuted`, user.user.id)
+    let muted = await client.mongo.get(`isMuted`, user.id)
     if (!muted) return message.channel.send(new Discord.MessageEmbed().setDescription(`<a:no:954773357407113298> | Sorry, but <@${user.id}> is not muted.`).setColor("RED"))
     if (!muteRole) return message.channel.send(new Discord.MessageEmbed().setDescription(`<a:no:954773357407113298> | I can't find \`Muted\` role in this guild`).setColor("RED"))
     
@@ -12,15 +12,14 @@ exports.run = async (client, message, args) => {
     .setColor("GREEN")
     .setAuthor(`Unmuted User`, `https://cdn.discordapp.com/emojis/742191092652310578.png?v=1`)
     .setThumbnail(`${message.author.displayAvatarURL({ dynamic: true, size: 4096 })}`)
-    .addField("**Warned User**", `${member} | \`${member.id}\``)
+    .addField("**Unmuted User**", `${user} | \`${user.id}\``)
     .addField("**Moderator**", `${author} | \`${author.id}\``)
-    .addField("**Reason**", `\`\`\`\n${reason}\n\`\`\``)
     .addField("**Timestamp**", `**\`\`\`css\n${new Date(message.createdTimestamp).toString()}\n\`\`\`**`)
     .setTimestamp();
 
     user.roles.remove(muteRole)
     client.channels.cache.get(client.config.modsChannel).send(embed)
-    bot.mongo.delete(`isMuted`, user.user.id);
+    bot.mongo.delete(`isMuted`, user.id);
     message.channel.send(new Discord.MessageEmbed().setDescription(`<a:yes:954773528153059350> | <@${user.id}> has been unmuted.`).setColor("GREEN"))
     
   }

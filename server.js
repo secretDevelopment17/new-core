b497bcceb269f4348cab0ead166d5095bcf533b2
Dezkrazzer <uqie.vanmadiun@gmail.com>
2022-03-19 20:02:42 +0700

    y

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.get("/case", function (req, res) {
 });
 
 app.set('json spaces', 2);
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });

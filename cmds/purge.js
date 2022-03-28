f2987473ca0caf8813a231ea54c638e56e29b601
Dezkrazzer <uqie.vanmadiun@gmail.com>
2022-03-28 17:21:25 +0700

    feat: purge command

---
+++ cmds/purge.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+  if (
+    !message.member.hasPermission("MANAGE_MESSAGES") &&
+    !message.member.roles.cache.some((r) => r.name === "sudo")
+  ) {
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setDescription(
+          `<a:no:954773357407113298> | I'm sorry but you don't have permission to do that.`
+        )
+        .setColor("RED")
+    );
+  }
+  if (!args[0])
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setDescription(
+          `<a:no:954773357407113298> | You must provide amount to delete chat.`
+        )
+        .setColor("RED")
+    );
+  message.channel.bulkDelete(args[0]).then(() => {
+    message.channel
+      .send(
+        new Discord.MessageEmbed()
+          .setDescription(
+            `<a:yes:954773528153059350> | Deleted **${args[0]}** chats`
+          )
+          .setColor("GREEN")
+      )
+      .then((msg) => msg.delete(5000));
+  });
+};
+
+exports.help = {
+  name: "purge",
+  aliases: [],
+};

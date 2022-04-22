f01983e2759635b1861df5eaf30ba0518af4efa8
Dezkrazzer <uqie.vanmadiun@gmail.com>
2022-04-22 18:38:52 +0700

    feat: invite command

---
+++ cmds/invite.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args) => {
+    try {
+        let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+
+        let invites = await message.guild.fetchInvites()
+
+        let memberInvites = invites.filter(i => i.inviter && i.inviter.id === member.user.id);
+
+        if (memberInvites.size <= 0) {
+            return message.channel.send(`<a:no:954773357407113298> | **${member.displayName}** never invited anyone to this server!`, (member === message.member ? null : member));
+{}          }
+
+        let content = memberInvites.map(i => i.code).join("\n");
+        let index = 0;
+        memberInvites.forEach(invite => index += invite.uses);
+
+        let embed = new Discord.MessageEmbed()
+            .setColor("GREEN")
+            .setFooter(message.guild.name, message.guild.iconURL())
+            .setAuthor(`Invite Tracker for ${message.guild.name}`)
+            .setDescription(`Information on Invites of ${member.displayName}`)
+            .addField("**No. Invited Persons**", index)
+            .addField("Invitation Codes\n\n", content);
+        message.channel.send(embed);
+    } catch (e) {
+        return message.channel.send(e.stack)
+    }
+}
+  
+  exports.help = {
+    name: "invite",
+    aliases: ["checkinv", "inv"]
+  }

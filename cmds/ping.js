f218d74573409ced5d6e43d1f8ffd7477f29b04f
unknown <uqie.vanmadiun@gmail.com>
2023-08-05 11:16:15 +0700

    chore: edited ping command

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,8 +1,32 @@
 exports.run = async (client, message, args) => {
-  message.channel.send(`ℹ | My latency is ${client.ws.ping}ms`)
-}
+//  message.channel.send(`ℹ | My latency is ${client.ws.ping}ms`)
+//}
 
+const start = Date.now();
+  const msgs = await message.channel.send("<a:DiscordLoading:593252708404690961> Pinging my server...");
+  const latency = Date.now() - start;
+  const embed = new Discord.MessageEmbed()
+    .setColor("#2f3136")
+    .setAuthor("🏓 PONG!" )
+    .addField(":signal_strength: | Latency:", `\`${latency}\`ms`, true)
+    .addField(
+      ":globe_with_meridians: | Websockets:",
+      `\`${client.ws.ping}\`ms`,
+      true
+    )
+    .setTimestamp();
+
+  if (client.ws.ping <= "100") {
+    embed.setColor("#21f508");
+  } else if (client.ws.ping <= "300") {
+    embed.setColor("#f7a53d");
+  } else if (client.ws.ping >= "300") {
+    embed.setColor("#f70505");
+  }
+  msgs.edit(embed);
+
+}
 exports.help = {
   name: "ping",
-  aliases: []
+  aliases: ["p"]
 }

01331f578d71d401af1bf0fc0bbc2f7d3b180c48
unknown <uqie.vanmadiun@gmail.com>
2024-03-08 22:47:27 +0700

    chore: edited rules

--- cmds/rules.js
+++ cmds/rules.js
@@ -8,13 +8,13 @@ exports.run = async (client, message, args) => {
     .setDescription(`
         - No spamming, flooding, or self-promotion\n
         - Don't share NSFW content on random channels\n
-        - Don't direct message anyone unless you already know them personally
-        - Abusing is allowed but stay in your limits
+        - Don't direct message anyone unless you already know them personally\n
+        - Abusing is allowed but stay in your limits\n
         - Last but not the least, be respectful of others! This includes but not limited to, refrain from being toxic, using hate speech, racism and sexual harassment\n
         \`\`\`Simple rules, but every staff is different, staff can take any action and whenever they want\`\`\`
     `)
     .addField("Note:", "> Keep in mind that our servers have smart AI designed to protect our servers. Our AI works without sleep (he is not human), automod works non-stop every day (unless there are some things)")
-    .addField("Invite Link:", "> https://discord.gg/tJCQZVwGaT")
+    .addField("Invite Link:", "**https://dsc.gg/finix**")
     .setFooter("Regards, Admin")
 
     client.channels.cache.get(client.config.rulesChannel).send(RULES)
